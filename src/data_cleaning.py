f2516160c4b98d2cf4819cf8efc3bee566368919
asifjawad <jawad.asif@studenti.unipd.it>
2023-12-23 15:17:08 +0100

    Data Cleaning

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,58 +1,90 @@
 import logging
-
-import pandas as pd
-import numpy as np
+from abc import ABC, abstractmethod
 from typing import Union
 
-from abc import ABC, abstractmethod
+import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
 class DataStrategy(ABC):
     """
-    Abstract Class to handle strategy and Data
+    Abstract Class defining strategy for handling data
     """
+
     @abstractmethod
     def handle_data(self, data: pd.DataFrame) -> Union[pd.DataFrame, pd.Series]:
         pass
 
 
-class DataProcessingStrategy(DataStrategy):
-    """ Pre-processing Data"""
-def handle_data(self, data: pd.DataFrame) -> pd.DataFrame:
-    try:
-        data = data.drop([], axis=1)
-        data = data.select_dtype(include=[np.number])
-        cols_to_drop = ["", ""]
-        data = data.drop(cols_to_drop, axis=1)
-        return data
-         
+class DataPreprocessStrategy(DataStrategy):
+    """
+    Data preprocessing strategy which preprocesses the data.
+    """
 
+    def handle_data(self, data: pd.DataFrame) -> pd.DataFrame:
+        """
+        Removes columns which are not required, fills missing values with median average values, and converts the data type to float.
+        """
+        try:
+            data = data.drop(
+                [
+                    "order_approved_at",
+                    "order_delivered_carrier_date",
+                    "order_delivered_customer_date",
+                    "order_estimated_delivery_date",
+                    "order_purchase_timestamp",
+                ],
+                axis=1,
+            )
+            data["product_weight_g"].fillna(data["product_weight_g"].median(), inplace=True)
+            data["product_length_cm"].fillna(data["product_length_cm"].median(), inplace=True)
+            data["product_height_cm"].fillna(data["product_height_cm"].median(), inplace=True)
+            data["product_width_cm"].fillna(data["product_width_cm"].median(), inplace=True)
+            # write "No review" in review_comment_message column
+            data["review_comment_message"].fillna("No review", inplace=True)
 
-    except Exception as e:
-        logging.error(f" Error in Processing Data {e}")
-        raise e
-    
+            data = data.select_dtypes(include=[np.number])
+            cols_to_drop = ["customer_zip_code_prefix", "order_item_id"]
+            data = data.drop(cols_to_drop, axis=1)
+
+            return data
+        except Exception as e:
+            logging.error(e)
+            raise e
+
 
 class DataDivideStrategy(DataStrategy):
+    """
+    Data dividing strategy which divides the data into train and test data.
+    """
 
     def handle_data(self, data: pd.DataFrame) -> Union[pd.DataFrame, pd.Series]:
+        """
+        Divides the data into train and test data.
+        """
         try:
-            X= data.drop([], axis= 1)
-            y = data[]
-            X_train, X_test, y_train, y_test = train_test_split(X,y)
-            retun X_train
-
+            X = data.drop("review_score", axis=1)
+            y = data["review_score"]
+            X_train, X_test, y_train, y_test = train_test_split(
+                X, y, test_size=0.2, random_state=42
+            )
+            return X_train, X_test, y_train, y_test
         except Exception as e:
-            logging.error(f)
+            logging.error(e)
             raise e
 
 
 class DataCleaning:
-    def __init__(self, df, strategy):
-        self.df = df
+    """
+    Data cleaning class which preprocesses the data and divides it into train and test data.
+    """
+
+    def __init__(self, data: pd.DataFrame, strategy: DataStrategy) -> None:
+        """Initializes the DataCleaning class with a specific strategy."""
+        self.df = data
         self.strategy = strategy
 
     def handle_data(self) -> Union[pd.DataFrame, pd.Series]:
-        try:
-            return self.strategy.handle_data(self.df)
+        """Handle data based on the provided strategy"""
+        return self.strategy.handle_data(self.df)
