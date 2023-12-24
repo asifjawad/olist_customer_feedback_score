a065d31cee9059e674aed99222ff9cd942672086
asifjawad <jawad.asif@studenti.unipd.it>
2023-12-24 13:51:11 +0100

    Evaluating Model

---
+++ src/evaluation.py
@@ -0,0 +1,52 @@
+from abc import ABC, abstractmethod
+import logging
+import numpy as np
+
+from sklearn.metrics import mean_squared_error, r2_score
+
+
+
+
+
+
+class Evaluation(ABC):
+    @abstractmethod
+    def calculate_score(self, y_test: np.ndarray, y_pred: np.ndarray):
+        pass
+
+
+class MSE(Evaluation):
+    def calculate_score(self, y_test: np.ndarray, y_pred: np.ndarray):
+        try:
+            logging.info(f"Calculating MSE")
+            mse = mean_squared_error(y_test, y_pred)
+            logging.info(f"MSE {mse}")
+            return mse
+        except Exception as e:
+            logging.error(f"Error calculating MSE")
+            raise e
+        
+
+
+class R2(Evaluation):
+    def calculate_score(self, y_test: np.ndarray, y_pred: np.ndarray):
+        try:
+            logging.info(f"Calculating MSE")
+            r2 = r2_score(y_test, y_pred)
+            logging.info(f"R2 {r2}")
+            return r2
+        except Exception as e:
+            logging.error(f"Error calculating R2")
+            raise e
+        
+
+class RMSE(Evaluation):
+    def calculate_score(self, y_test: np.ndarray, y_pred: np.ndarray):
+        try:
+            logging.info(f"Calculating MSE")
+            rmse = r2_score(y_test, y_pred, squared=False)
+            logging.info(f"R2 {rmse}")
+            return rmse
+        except Exception as e:
+            logging.error(f"Error calculating RMSE")
+            raise e
--- steps/evaluation.py
+++ steps/evaluation.py
@@ -2,7 +2,36 @@ import logging
 
 import pandas as pd
 from zenml import step
+from sklearn.base import RegressorMixin
+
+from typing import Tuple
+from typing_extensions import Annotated
+
+from src.evaluation import RMSE, MSE, R2
 
 @step
-def evaluate_model(df: pd.DataFrame) -> None:
-    pass
+def evaluate_model(
+    model: RegressorMixin,
+    X_test: pd.DataFrame,
+    y_test:pd.DataFrame
+                   ) -> Tuple[
+                       Annotated[float, "mse"],
+                       Annotated[float,"r2"]
+                   ]:
+    
+
+    try:
+        prediction = model.predict(X_test)
+        mse_class= MSE()
+        mse = mse_class.calculate_score(y_test, prediction)
+
+        r2_class= R2()
+        r2 = r2_class.calculate_score(y_test, prediction)
+
+        rmse_class= RMSE()
+        rmse = rmse_class.calculate_score(y_test, prediction)
+
+        return mse, r2, rmse
+    except Exception as e:
+        logging.error(f"Error in evaluating models")
+        raise e
